80ab3001a32ed49032c10e2f01ad2a95faf2fa73
meet447 <meet.sonawane2015@gmail.com>
2026-06-20 02:42:02 +0530

    Remove dead code and feature Koraku, Chipling, and MeuxCompanion.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import { lazy, Suspense } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "next-themes";
@@ -33,23 +29,15 @@ const AnimatedRoutes = () => {
   );
 };
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <NavBar />
-          <CommandPalette />
-          <AnimatedRoutes />
-          <AgentLink />
-        </BrowserRouter>
-      </ThemeProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+    <BrowserRouter>
+      <NavBar />
+      <CommandPalette />
+      <AnimatedRoutes />
+      <AgentLink />
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default App;
